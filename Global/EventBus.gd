bef212d7cf8d5390603a69a6339848f393cd77e2
skythe7 <skythe761@gmail.com>
2026-07-23 11:56:27 +0700

    Add input_dialogue as a parameter for the signal

--- Global/EventBus.gd
+++ Global/EventBus.gd
@@ -3,4 +3,4 @@ extends Node
 #These exist strictly to show how an eventbus works. 
 signal score_changed(new_score: int)
 signal chinchilla_pooped(poop_count: int, poop_position: Vector2)
-signal dialogue_triggered
+signal dialogue_triggered(input_dialogue: Array[String])
--- Scripts/UI/Dialogue/dialogue_box.gd
+++ Scripts/UI/Dialogue/dialogue_box.gd
@@ -6,14 +6,17 @@ signal nextDialogue
 @onready var mood_label: Label = $Panel/MoodLabel
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
-@export var dialogue_messages: Array[String]
+func _ready() -> void:
+	EventBus.dialogue_triggered.connect(_on_dialogue_triggered)
 
 func _input(event: InputEvent) -> void:
+	# For whenever player press primary key, it skips to the next dialogue
 	if event.is_action_pressed("action_primary"):
 		nextDialogue.emit()
 
-func _ready() -> void:
-	for message in dialogue_messages:
+func _on_dialogue_triggered(input_dialogue) -> void:
+	# Basic dialogue system
+	for message in input_dialogue:
 		dialogue_label.text = message
 		animation_player.play("typing_anim")
 		await nextDialogue
