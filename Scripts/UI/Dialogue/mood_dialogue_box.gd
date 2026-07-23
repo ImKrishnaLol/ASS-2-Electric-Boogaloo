ab507257ebabf8e57e9978fab111d43e12ad3d4a
CuriousBlobfish <blobfishcurious@gmail.com>
2026-07-23 20:10:18 +0100

    refactored dialogue box to display any kind of dialogue on receieving a signal and hide it again after next dialogue signal is emitted, commented out level dialogue, created mood dialogue box

---
+++ Scripts/UI/Dialogue/mood_dialogue_box.gd
@@ -0,0 +1,36 @@
+extends Control
+
+@onready var dialogue_box: DialogueBox = $DialogueBox
+
+var dialogue_moods: Dictionary = {
+	"HAPPY": [
+		"Im happy",
+		"*happy noise*",
+		"Yippyyyyy",
+		"MATH IS FUN",
+	],
+	"FLIRTY": [
+		"I lik u lowkey",
+		"Are you Wi‑Fi? Because I’m feeling strong signals and zero common sense. (AI)",
+		"How do i rizz someone",
+		"touch grass? no. touch me (WHY IS MY AI FREAKY GAHDAMN)",
+	],
+	"ANGRY": [
+		"BARK BARK BARK BARK GRRRRR",
+		"D:<",
+		"GRRRR BARK",
+	],
+	"DEJECTED": [
+		"MC, I know we don't get along.. but I think I'm cooked.",
+		"Um.. my rizz is on airplane mode. (AI GENERATED)",
+	],
+}
+
+
+func _ready() -> void:
+	EventBus.dialogue_mood_triggered.connect(_on_dialogue_mood_triggered)
+
+func _on_dialogue_mood_triggered(mood: String, level: int) -> void:
+	# Dialogue system for mood mode
+	var dialogue : String = dialogue_moods[mood].pick_random()
+	dialogue_box.DISPLAY_DIALOGUE.emit(dialogue)
