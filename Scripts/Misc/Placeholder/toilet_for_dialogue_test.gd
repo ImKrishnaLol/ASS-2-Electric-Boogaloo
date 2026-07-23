02731f73fc04fdf6d67be58ece18a3232f6124d4
skythe7 <skythe761@gmail.com>
2026-07-23 12:57:10 +0700

    Change the export moods mode into export enum

--- Scripts/Misc/Placeholder/toilet_for_dialogue_test.gd
+++ Scripts/Misc/Placeholder/toilet_for_dialogue_test.gd
@@ -1,7 +1,7 @@
 extends TextureButton
 
 # Inputs for the dialogue
-@export var mood: String
+@export_enum("happy", "sad", "angry", "neutral") var mood: String
 
 func _on_pressed() -> void:
 	EventBus.dialogue_triggered.emit(mood)
