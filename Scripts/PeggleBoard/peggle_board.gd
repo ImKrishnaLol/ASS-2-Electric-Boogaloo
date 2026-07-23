cd1b757d040c8e9545d757d9098818c4fc7f5c82
Desephalo <desepublicwork@gmail.com>
2026-07-23 12:35:20 +0100

    Made the peggle cannon turn.
    I added some assets for the peggle cannon, made it turn, and added a limmit on how much it can turn.
    
    I might come back to this and add some game feel.
    
    The projectiles also aren't clamped and will fly up despite the cannon not turning. Might fix that to.
    
    Note: I want to throw Github out the window. It cost me half of my break.

--- Scripts/PeggleBoard/peggle_board.gd
+++ Scripts/PeggleBoard/peggle_board.gd
@@ -7,7 +7,8 @@ extends Node2D
 
 # SCENES
 @export var ball: PackedScene
-@onready var peggle_ball_shooter: Sprite2D = $PeggleBallShooter
+@onready var peggle_ball_shooter: Node2D = $PeggleBallShooter
+@onready var peggle_ball_firing_point: Node2D = $PeggleBallShooter/PeggleBallBarrel/PeggleBallFiringPoint
 
 # VARIABLES (exports)
 @export var shoot_offset: Vector2; # how far from shooter balls should spawn
@@ -16,6 +17,14 @@ extends Node2D
 
 func _ready() -> void:
 	endzone.body_entered.connect(destroy_ball)
+	peggle_ball_shooter.rotation = 90
+
+func _process(_delta: float) -> void:
+	peggle_ball_shooter.look_at(get_global_mouse_position())
+	peggle_ball_shooter.rotation = clampf(
+		peggle_ball_shooter.rotation,
+		deg_to_rad(0),
+		deg_to_rad(180))
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("action_primary"): # space/left click
@@ -24,13 +33,14 @@ func _input(event: InputEvent) -> void:
 func shoot_ball() -> void:
 	var new_ball = ball.instantiate()
 	get_tree().current_scene.add_child(new_ball)
-	new_ball.global_position = peggle_ball_shooter.global_position + shoot_offset
+	new_ball.global_position = peggle_ball_firing_point.global_position + shoot_offset
 	# make it move in a direction
 	new_ball.apply_central_impulse(shoot_strength * get_direction_to_mouse())
 
 func get_direction_to_mouse() -> Vector2:
 	var mouse_position = get_global_mouse_position()
 	var direction = peggle_ball_shooter.global_position.direction_to(mouse_position)
+	print(direction)
 	return direction
 
 func destroy_ball(body: Node2D) -> void:
