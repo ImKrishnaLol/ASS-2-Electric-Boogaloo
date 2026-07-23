87232d8104609dab2665f1fbe20fc59c27afd2fc
JeroxiaDev <Jeroxia@gmail.com>
2026-07-23 17:43:21 +0200

    Updated main menu
    - New background shaders
    - Transferred to main
    - Removed some duplicate files causing conflicst.

--- Global/EventBus.gd
+++ Global/EventBus.gd
@@ -1,5 +1,5 @@
 extends Node
 
 #These exist strictly to show how an eventbus works. 
-signal score_changed(new_score: int)
-signal chinchilla_pooped(poop_count: int, poop_position: Vector2)
+#signal score_changed(new_score: int)
+#signal chinchilla_pooped(poop_count: int, poop_position: Vector2)
